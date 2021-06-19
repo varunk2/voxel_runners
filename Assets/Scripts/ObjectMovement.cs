05a846d6a43a0e9d2c5a8961a416414aa21dddfe
Varun Sharma <varunk22110@gmail.com>
2021-06-19 15:54:10 +0530

    Completed TreeLine repeatition and adjusted the camera.

--- Assets/Scripts/ObjectMovement.cs
+++ Assets/Scripts/ObjectMovement.cs
@@ -8,18 +8,16 @@ public class ObjectMovement : MonoBehaviour
 
     void Start()
     {
-        /*_objectSpeed = GameManager._worldSpeed;
-        Debug.Log(_objectSpeed);*/
     }
 
     void Update()
     {
         if (GameManager._canMove) {
             transform.position -= new Vector3(0f, 0f, (GameManager._worldSpeed * Time.deltaTime));
-            //transform.position -= new Vector3(0f, 0f, (_objectSpeed * Time.deltaTime));
         }
 
         if (transform.position.z < PathDestructionPoint._zPosition) {
+            //Debug.Log("Destruction point: " + transform.position.z);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/TreeLineMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeLineMovementController : MonoBehaviour
+{
+    [SerializeField] private Transform _disappearPoint;
+    [SerializeField] private BoxCollider _boxCollider;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (GameManager._canMove) {
+            transform.position -= new Vector3(0f, 0f, (GameManager._worldSpeed * Time.deltaTime));
+        }
+
+        if (transform.position.z < _disappearPoint.position.z) {
+            Debug.Log("Reposition point: " + transform.position.z);
+            transform.position += new Vector3(0f, 0f, _boxCollider.bounds.size.z * 2f);
+        }
+    }
+}
